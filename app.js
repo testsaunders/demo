213ea6d2e41ec287b58c732e2e83e221598ccb82
= <=>
2018-01-06 13:15:41 -0700

    add console log

--- app.js
+++ app.js
@@ -3,5 +3,6 @@
 
     console.log('This is a github demo, and I just added text!!');
     console.log('This is from the MASTER branch');
+    console.log('this is some new changes!!');
     console.log('This is more text being added from the adding-text branch');
 }());
