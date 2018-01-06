f3a4c47ab388e45be440493b5199f32d3b2fc86b
= <=>
2018-01-06 13:04:43 -0700

    add new console log

--- app.js
+++ app.js
@@ -2,5 +2,6 @@
     'use strict';
 
     console.log('This is a github demo, and I just added text!!');
-    // This is adding-text branch
+    console.log('This is more text being added from the adding-text branch');
+    // This is adding-text branch.
 }());
